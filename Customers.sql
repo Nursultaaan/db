e97a507a212d961e426efbfaac9f18ae701ba53a
lovethempepegas <149992468+lovethempepegas@users.noreply.github.com>
2023-11-06 11:04:34 +0600

    v1.0

---
+++ Customers.sql
@@ -0,0 +1,17 @@
+create table Customers (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	address VARCHAR(50)
+);
+insert into Customers (id, first_name, last_name, email, address) values (1, 'Nevin', 'Millar', 'nmillar0@japanpost.jp', 'Suite 51');
+insert into Customers (id, first_name, last_name, email, address) values (2, 'Jeffy', 'Knewstub', 'jknewstub1@redcross.org', 'Room 755');
+insert into Customers (id, first_name, last_name, email, address) values (3, 'Ottilie', 'Konmann', 'okonmann2@people.com.cn', 'PO Box 11621');
+insert into Customers (id, first_name, last_name, email, address) values (4, 'Doro', 'Whisby', 'dwhisby3@deliciousdays.com', 'Apt 110');
+insert into Customers (id, first_name, last_name, email, address) values (5, 'Lindsey', 'Morlon', 'lmorlon4@nbcnews.com', 'Apt 916');
+insert into Customers (id, first_name, last_name, email, address) values (6, 'Noland', 'Hambly', 'nhambly5@creativecommons.org', 'Room 1313');
+insert into Customers (id, first_name, last_name, email, address) values (7, 'Angelina', 'Einchcombe', 'aeinchcombe6@smh.com.au', 'Apt 1069');
+insert into Customers (id, first_name, last_name, email, address) values (8, 'Patricia', 'Mozzi', 'pmozzi7@nifty.com', 'Room 1146');
+insert into Customers (id, first_name, last_name, email, address) values (9, 'Reg', 'Kingswoode', 'rkingswoode8@samsung.com', 'Suite 65');
+insert into Customers (id, first_name, last_name, email, address) values (10, 'Clarence', 'Ahrendsen', 'cahrendsen9@ihg.com', 'Room 1699');
