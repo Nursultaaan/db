e6c2fa9168f7478f5c546caba5449583c6b191e3
unknown <Admin@DESKTOP-C5GE7KN.aitu.kz>
2023-11-09 13:08:50 +0600

    v1.3

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import psycopg2
 
 conn = psycopg2.connect(
     host = "localhost",
-    database = "postgres",
+    database = "Customers",
     user = "postgres",
     password = "123456" #v1.2
 )
  
 cur = conn.cursor()
+print("Connected succesfully!")
+cur.execute("SELECT first_name FROM customers")
+usernames = [r[0] for r in cur.fetchall()]
+username = input("Enter your username: ")
+if username in usernames:
+    print("Your username exist in our database")
+else:
+    print("Error! Couldn't find your username in database.")
