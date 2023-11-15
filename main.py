d39f8a8234a7f06625bd3b08aea9c302c05cde82
Stem Stem <stem@astanait.edu.kz>
2023-11-15 09:36:27 +0600

    v1.5

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
 #v1.3
 import psycopg2
+import csv 
 
 conn = psycopg2.connect (
     host = "localhost",
-    database = "Customers",
+    database = "postgres",
     user = "postgres",
     password = "1234567" 
 )
  
 cur = conn.cursor()
 print("Connected successfully!") 
-cur.execute("SELECT first_name FROM customers ORDER BY first_name ASC;")
-usernames = [r[0] for r in cur.fetchall()]
+cur.execute("SELECT * FROM customers")
+usernames = cur.fetchall()
+for i in usernames:
+    print(i)
+
+filename = "users.csv"
+
+with open(filename, 'w', newline = '') as csvfile:
+    csvwriter = csv.writer(csvfile)
+
+    csvwriter.writerows(usernames)
+
 # username = input("Enter your username: ")
 # if username in usernames:
 #     print("Your username exist in our database")
 # else:
 #     print("Error! Couldn't find your username in our database.")
 
-def binary_search(username):
-    low = 0 
-    high = len(usernames) - 1
+# def binary_search(username):
+#     low = 0 
+#     high = len(usernames) - 1
 
-    while low <= high:
-        mid = (low + high) // 2
-        guess = usernames[mid]
+#     while low <= high:
+#         mid = (low + high) // 2
+#         guess = usernames[mid]
 
-        if guess == username:
-            print(f"Found username {username} in the database")
-            return 
-        elif guess < username:
-            low = mid + 1
-        else:
-            high = mid - 1
+#         if guess == username:
+#             print(f"Found username {username} in the database")
+#             return 
+#         elif guess < username:
+#             low = mid + 1
+#         else:
+#             high = mid - 1
 
-    print(f"Username {username} not found in the database")
+#     print(f"Username {username} not found in the database")
 
-username = input("Enter your username: ")
-print(usernames)
-binary_search(username)
+# username = input("Enter your username: ")
+# print(usernames)
+# binary_search(username)
